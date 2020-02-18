a403ef8f9fa9b055e0c5d50270594b1c0d444f7b
tyler jones <rteyl1690@gmail.com>
2020-02-18 04:24:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(string)
   new_array = []
   array = string.split(/ /)
   array.collect do |word| word.reverse! end
-    array.join(" ") >> new_array
+    array.join(" ")
   end
