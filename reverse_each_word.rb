66509352c67dc413358323761c4c58c184a41776
tyler jones <rteyl1690@gmail.com>
2020-02-18 04:21:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,8 @@ def reverse_each_word(string)
   array = string.split(/ /)
   array.each do |word| word.reverse! end
     do array.join(" ") end
+    do
   array.collect do |word| word.reverse! end
     new_array << array.join(" ")
+  end
   end
